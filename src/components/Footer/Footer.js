c23239d4f097c56bb30d55aa1d66df643f105c0f
hayhaydz <haydon1900@gmail.com>
2020-09-10 11:25:50 +0100

    Footer and Logo

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="Footer">
+            <p className="Footer__copyright">Temopus © 2020</p>
+        </footer>
+    )
+}
+export default Footer
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../styles/main.scss';
 
 import Header from '../Header/Header'
+import Footer from '..//Footer/Footer'
 
 const Layout = ({ children }) => {
     return (
         <main className="Layout">
             <Header />
             {children}
+            <Footer />
         </main>
     )
 }
