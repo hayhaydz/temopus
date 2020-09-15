25a8ad7f179f2d3583bcfee5359a8bd6ba77b950
hayhaydz <haydon1900@gmail.com>
2020-09-15 10:28:45 +0100

    Blog Post

--- src/templates/project-post.js
+++ src/templates/project-post.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-const ProjectPost = () => {
+import Layout from '../components/Layout/Layout'
+
+const ProjectPost = ({ data }) => {
     return (
-        <h1>This is a project.</h1>
+        <Layout>
+            <section className="ProjectPost">
+                <span className="ProjectPost__smalltitle">Title</span>
+                <h1 className="ProjectPost__title heading">{data.markdownRemark.frontmatter.title}</h1>
+                <span className="ProjectPost__smalltitle">Completion Date</span>
+                <h2 className="ProjectPost__date">{data.markdownRemark.frontmatter.date}</h2>
+                <span className="ProjectPost__smalltitle">Description</span>
+                <p className="ProjectPost__desc">{data.markdownRemark.frontmatter.description}</p>
+                <div className="ProjectPost__content" dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}/>
+            </section>
+        </Layout>
     )
 }
 export default ProjectPost
+
+export const pageQuery = graphql`
+    query ProjectPostBySlug($slug: String) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            frontmatter {
+                title
+                description
+                date(formatString: "DD/MM/YYYY")
+            }
+            html
+        }
+    }
+`
